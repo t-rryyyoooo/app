675f9cea5ebe12183a4b4b0fec19cdc02e481d17
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2020-04-15 23:47:37 +0900

    I complete to make form to add menu.

--- muscle/views.py
+++ muscle/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Menu
 from collections import defaultdict
 from . import PARTS
+from .forms import AddMenuForm
 
 def recordIndexFunc(request):
+    print(request.method)
+    form = AddMenuForm(request.POST or None)
+    print(form)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        print(form)
+        print("OK")
+        return redirect("recordIndex")
+
+    print("NO")
     objects = {}
     for part in PARTS:
         objects[part] = Menu.objects.filter(part = part)
-
 
     contexts = {
             "objects" : objects
@@ -21,5 +31,10 @@ def recordDetailFunc(request, part):
 
     return render(request, "recordDetail.html", {"objects" : objects})
 
-def addMenuFunc(request):
-    return HttpResponse("addMenu")
+def addMenuFunc(request, part = None):
+
+    contexts = {
+            "form" : AddMenuForm(), 
+            "part" : part
+            }
+    return render(request, "addMenu.html", contexts)
